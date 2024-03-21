70069ff4f0729a6f52573e257fd17f04659903f4
Damian Kluczyński <damiankluczynski00@gmail.com>
2024-03-21 01:13:12 +0100

    added data labeling

---
+++ data_preprocessing.py
@@ -0,0 +1,44 @@
+import os
+import pickle
+import numpy as np
+from PIL import Image
+
+# data paths
+train_path = 'data/train'
+validation_path = 'data/validation'
+
+
+# prepare labels for data
+def prepare_data(path):
+    data = {'images': [], 'labels': []}
+    categories = {'cars': 1, 'non_cars': 0}
+    for category, label in categories.items():
+        categories_path = os.path.join(path, category)
+        for file in os.listdir(categories_path):
+            full_path = os.path.join(categories_path, file)
+            image = Image.open(full_path)
+            image = np.array(image)
+            data['images'].append(image)
+            data['labels'].append(label)
+    return data
+
+
+# save prepared labels in pickle
+def save_data(data, file):
+    file = 'data/' + file
+    with open(file, 'wb') as f:
+        pickle.dump(data, f)
+
+
+# load prepared data from pickle
+def load_data(file):
+    file = 'data/' + file + '.pkl'
+    with open(file, 'rb') as f:
+        return pickle.load(f)
+
+
+train_data = prepare_data(train_path)
+save_data(train_data, 'train_data')
+
+validation_data = prepare_data(validation_path)
+save_data(validation_data, 'validation_data')
